950b98c7e884353ff5a3c07cd1dbd88814679fca
Claudio Dellavedova <claudio.dellavedova@netizens.it>
2019-02-12 18:13:00 +0100

    12/02/2019

--- pyRunner.php
+++ pyRunner.php
@@ -12,16 +12,16 @@
     // remove the part that we don't need from the provided image and decode it
     $imgdata = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST['imagebase64']));
     
-    $imgfilepath = "userimages/image.jpg";
+    $imgfilepath = "E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/new_web_app/2019-01-19008-throadshow-new_web_app/userimages/image.jpg";
 
     file_put_contents($imgfilepath,$imgdata);
 
     $WshShell = new COM("WScript.Shell"); 
-    $oExec = $WshShell->Run("python.exe E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/tf_files/tf_files_bw/label_image.py --graph E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/tf_files/tf_files_bw/retrained_graph.pb --labels E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/tf_files/tf_files_bw/retrained_labels.txt --input_layer Placeholder --output_layer final_result --image E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/new_web_app/userimages/image.jpg", 7, false);
+    $oExec = $WshShell->Run("python.exe E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/tf_files/tf_files_bw/label_image.py --graph E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/tf_files/tf_files_bw/retrained_graph.pb --labels E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/tf_files/tf_files_bw/retrained_labels.txt --input_layer Placeholder --output_layer final_result --image E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/new_web_app/2019-01-19008-throadshow-new_web_app/userimages/image.jpg", 7, false);
 
     //While per ricerca esistenza file output.txt
     //Quando trovato, leggere il contenuto, interpretarlo se necessario e inviare la risposta al client
-    $outputpath = "userimages/output.txt";
+    $outputpath = "E:/Claudio/Progetti/2019/2019-01-19008-THRoadshow/new_web_app/2019-01-19008-throadshow-new_web_app/userimages/output.txt";
     while(!file_exists($outputpath)){
 
     }
@@ -31,12 +31,20 @@
         $perc = floatval(fgets($outputfile));
         if($perc < 0.85){
             $string = fgets($outputfile);
+            fclose($outputfile);
+            unlink($outputpath);
             echo $string;
         }
-        else echo $string;
+        else {
+            fclose($outputfile);
+            unlink($outputpath);
+            echo $string;
+        }
     }
-    else echo $string;
-    fclose($outputfile);
-    unlink("userimages/output.txt");
+    else {
+        fclose($outputfile);
+        unlink($outputpath);
+        echo $string;
+    }
 
 ?>
